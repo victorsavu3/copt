9c1651551a590e385d78414a9be1ff012c389277
Victor Savu <victorsavu3@gmail.com>
2014-10-26 19:11:30 +0100

    implement transformation for conditions and memory operations

--- transform.ml
+++ transform.ml
@@ -22,5 +22,30 @@ module Memorization : S = struct (* applicative functor *)
         let te = t_expr e in
         [u, Assign (te, e), w;
          w, Assign (r, var te), v]
+    | u, Pos(e), v ->
+    	let w = nn () in
+        let te = t_expr e in
+        [u, Assign (te, e), w;
+         w, Pos(Lval(Var(te))), v]
+    | u, Neg(e), v ->
+    	let w = nn () in
+        let te = t_expr e in
+        [u, Assign (te, e), w;
+         w, Neg(Lval(Var(te))), v]
+    | u, Load(reg, e), v ->
+    	let w = nn () in
+        let te = t_expr e in
+        [u, Assign (te, e), w;
+         w, Load(reg, Lval(Var(te))), v]
+    | u, Store(e1, e2), v ->
+    	let w1 = nn () in
+    	let w2 = nn () in
+        let te1 = t_expr e1 in
+        let te2 = t_expr e2 in
+        [u, Assign (te1, e1), w1;
+         w1, Assign (te2, e2), w2;
+         w2, Store(Lval(Var(te1)), Lval(Var(te2))), v]
+    | u, x, v ->
+      [u, x, v]
     | _ -> ?? "Exercise 2.3"
 end
