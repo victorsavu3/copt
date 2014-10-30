9fdf85d2fdefafc0b7c22c834cb9d51ebb447574
Ralf Vogler <ralf.vogler@gmail.com>
2014-10-30 17:06:38 +0100

    solution for exercise 2.3
    Conflicts:
    	transform.ml

--- transform.ml
+++ transform.ml
@@ -17,10 +17,33 @@ module Memorization : S = struct (* applicative functor *)
     | None -> (let r = nr () ^ "_memo" in Hashtbl.add memo_registers expr r; r)
 
   let transform = function
-    | u, Assign (r, e), v ->
-        let w = nn () in
+    | (u, Pos e, v) ->
+        let v1 = nn () in
         let te = t_expr e in
-        [u, Assign (te, e), w;
-         w, Assign (r, var te), v]
-    | _ -> ?? "Exercise 2.3"
+        [u, Assign (te, e), v1;
+        v1, Pos (var te), v]
+    | (u, Neg e, v) ->
+        let v1 = nn () in
+        let te = t_expr e in
+        [u, Assign (te, e), v1;
+        v1, Neg (var te), v]
+    | (u, Assign (r, e), v) ->
+        let v1 = nn () in
+        let te = t_expr e in
+        [u, Assign (te, e), v1;
+        v1, Assign (r, var te), v]
+    | (u, Load (r, e), v) ->
+        let v1 = nn () in
+        let te = t_expr e in
+        [u, Assign (te, e), v1;
+        v1, Load (r, var te), v]
+    | (u, Store (e1, e2), v) ->
+        let v1 = nn () in
+        let v2 = nn () in
+        let te1 = t_expr e1 in
+        let te2 = t_expr e2 in
+        [u, Assign (te1, e1), v1;
+        v1, Assign (te2, e2), v2;
+        v2, Store (var te1, var te2), v]
+    | (u, Skip, v) -> [u, Skip, v]
 end
