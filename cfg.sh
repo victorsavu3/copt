94871116b62c8cc5b3064109b116ce1f4b2e6dee
Ralf Vogler <ralf.vogler@gmail.com>
2014-11-21 17:29:44 +0100

    update cfg.sh to display multiple graphs at once

--- cfg.sh
+++ cfg.sh
@@ -1,1 +1,9 @@
-dot -Tpng | display png:-
+#!/bin/bash
+trap 'kill 0' SIGINT SIGTERM EXIT
+
+splitgraphs=($(cat | awk -vRS="}" '{gsub(/ |\t|\n/, ""); printf $0" "}'))
+for i in ${splitgraphs[@]}
+do
+	echo "$i}" | dot -Tpng | display png:- &
+done
+wait
