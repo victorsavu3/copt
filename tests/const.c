f38962e21af1fef68f60af3f65076a225ea64838
Victor Savu <victorsavu3@gmail.com>
2014-11-27 23:11:38 +0100

    add a test for constants

---
+++ tests/const.c
@@ -0,0 +1,27 @@
+int main() {
+	
+	int a = 2;
+	int b = 3;
+
+	int c = a+b;
+
+	int*d = 0;
+ 
+	d[1] = c;
+
+	int nc;
+
+	if(a == 1){
+		b = 2;
+	}
+
+	c = a+b;
+
+	if(nc) {
+		b = 7;
+	}
+
+	c = a+b;
+	
+	c = nc + 2;
+}
