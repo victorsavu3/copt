c75977b2c3d6509eb50274a2edbb593d66d9bca2
Ralf Vogler <ralf.vogler@gmail.com>
2014-11-27 22:24:30 +0100

    fix for cfg.sh from Patrick Werneck

--- cfg.sh
+++ cfg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 trap 'kill 0' SIGINT SIGTERM EXIT
 
-splitgraphs=($(cat | awk -vRS="}" '{gsub(/ |\t|\n/, ""); printf $0" "}'))
+splitgraphs=($(cat | awk -vRS="digraph" '{gsub(/ |\t|\n/, ""); printf $0" "}'))
 for i in ${splitgraphs[@]}
 do
-	echo "$i}" | dot -Tpng | display png:- &
+  echo "digraph$i" | dot -Tpng | display png:- &
 done
 wait
